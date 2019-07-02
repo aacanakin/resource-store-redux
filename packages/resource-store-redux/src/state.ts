b48365a702ff08d2d8db1548a2668e49cbe57aae
Aras Can Akin <aacanakin@gmail.com>
2019-07-02 22:01:52 +0300

    Use foreach instead of map

--- packages/resource-store-redux/src/state.ts
+++ packages/resource-store-redux/src/state.ts
@@ -2,10 +2,10 @@ import { ResourceStoreState } from ".";
 
 export function initialState(keys: string[]): ResourceStoreState {
   const state: ResourceStoreState = {};
-  keys.map((k) => {
-    state[k] = {
+  keys.forEach((key: string) => {
+    state[key] = {
       isBusy: false,
     };
-  });
+  })
   return state;
 }
