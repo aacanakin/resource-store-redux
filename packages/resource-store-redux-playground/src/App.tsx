1bb12a751353e0a674b8cb35a0eb7ce700677fbf
Aras Can Akin <aacanakin@gmail.com>
2019-07-13 01:32:01 +0300

    Cleanup playground app

--- packages/resource-store-redux-playground/src/App.tsx
+++ packages/resource-store-redux-playground/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<Box component="span" m={1}>
+				<Button />
+			</Box>
+			<Button>Hello</Button>
+		</div>
+	);
+};
 
 export default App;
