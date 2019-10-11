6167600a7e44e0cc051bacd52280d050e5de8beb
Aras Can Akin <aacanakin@gmail.com>
2019-10-11 22:13:55 +0300

    Refactor selectors using reselect

--- packages/resource-store-redux/src/selectors.ts
+++ packages/resource-store-redux/src/selectors.ts
@@ -1,24 +1,25 @@
+import { createSelector } from 'reselect';
 import { ResourceStoreState } from './types';
 
-export function getResource(state: ResourceStoreState, key: string) {
-  return state[key];
-}
+export const getResource = (state: ResourceStoreState, key: string) =>
+  state[key];
 
-export function getData(state: ResourceStoreState, key: string) {
-  const resource = getResource(state, key);
-  return resource ? resource.data : undefined;
-}
+export const getData = createSelector(
+  getResource,
+  resource => resource && resource.data,
+);
 
-export function isBusy(state: ResourceStoreState, key: string) {
-  const resource = getResource(state, key);
-  return resource ? resource.isBusy : undefined;
-}
+export const isBusy = createSelector(
+  getResource,
+  resource => resource && resource.isBusy,
+);
 
-export function getError(state: ResourceStoreState, key: string) {
-  const resource = getResource(state, key);
-  return resource ? resource.error : undefined;
-}
+export const getError = createSelector(
+  getResource,
+  resource => resource && resource.error,
+);
 
-export function hasError(state: ResourceStoreState, key: string) {
-  return getError(state, key) !== undefined;
-}
+export const hasError = createSelector(
+  getResource,
+  resource => resource && !!resource.error,
+);
